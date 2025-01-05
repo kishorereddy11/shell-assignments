be2e5f8776623de1418d20de282fab22bcf4fdd4
kishor <kishorebsc12@gmail.com>
2025-01-05 13:04:22 +0530

    example

--- readtextfile.sh
+++ readtextfile.sh
@@ -8,4 +8,4 @@ done < $1
 echo "====="
 
 
-tr -cs 'a-zA-Z' '\n' < "$1" | tr '[:upper:]' '[:lower:]' | sort | uniq -c
+tr -cs 'a-zA-Z' '\n' < "$1" | tr '[:upper:]' '[:lower:]' | sort | uniq -c | head -n 3
